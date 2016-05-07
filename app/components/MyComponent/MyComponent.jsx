1198bbf2dee40a544aac29ea0f6f9d606960b3a2
u 徐 <xw@udeMacBook-Pro.local>
2016-05-07 09:12:43 +0800

    init boilerplate

--- app/components/MyComponent/MyComponent.jsx
+++ app/components/MyComponent/MyComponent.jsx
@@ -6,9 +6,7 @@ class MyComponent extends Component {
   render() {
     return (
       <h1 className="demo">
-        <span className="glyphicon glyphicon-th"></span>
-        更多干货请前往：http://guoyongfeng.github.io/idoc/
-        <span className="glyphicon glyphicon-plane"></span>
+       component
       </h1>
     );
   }
--- app/containers/App/App.jsx
+++ app/containers/App/App.jsx
@@ -15,8 +15,7 @@ class App extends Component {
       <div>
         <MyComponent />
         <p>
-          欢迎在github上一起维护这个脚手架项目<br />
-          https://github.com/GuoYongfeng
+          containers
         </p>
       </div>
     );
