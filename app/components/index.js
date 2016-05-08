5ae81851ac80f48ab7bdbf56d5654fd7da299b3f
followfork <xw@udeMacBook-Pro.local>
2016-05-08 15:15:01 +0800

    add component --- UserProfile, UserRepos, Notes

--- app/components/index.js
+++ app/components/index.js
@@ -2,3 +2,6 @@
 
 export MyComponent from './MyComponent/MyComponent.jsx';
 export SearchGithub from './SearchGithub/SearchGithub.jsx';
+export UserProfile from './UserProfile/UserProfile.jsx';
+export UserRepos from './UserRepos/UserRepos.jsx';
+export Notes from './Notes/Notes.jsx';
--- app/containers/Profile/Profile.jsx
+++ app/containers/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { UserProfile, UserRepos, Notes } from '../../components'
 
 class Profile extends Component {
   render(){
@@ -6,12 +7,15 @@ class Profile extends Component {
       <div className="row">
         <div className="col-md-4">
           UserProfile 路由的参数是：{this.props.params.username}
+          <UserProfile />
         </div>
         <div className="col-md-4">
           UserRepos
+          <UserRepos />
         </div>
         <div className="col-md-4">
           Notes
+          <Notes />
         </div>
       </div>
     )
