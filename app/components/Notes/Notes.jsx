8f2f785c2b2e825d548f09caa85487b0d34abcc5
followfork <xuwei_james@163.com>
2016-05-08 18:57:55 +0800

    add core-decorators(es6 nonsupport mixin)   ,firebase notes data

--- app/components/Notes/Notes.jsx
+++ app/components/Notes/Notes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class Notes extends Component {
   render(){
+  	console.log('notes:', this.props.notes);
     return (
       <div> 
       	<p> 评论 </p>
--- app/containers/Profile/Profile.jsx
+++ app/containers/Profile/Profile.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { UserProfile, UserRepos, Notes } from '../../components'
+import { mixin } from 'core-decorators';
+import ReactFireMixin from 'reactfire';
+import Firebase from 'firebase';
 
+@mixin(ReactFireMixin)
 class Profile extends Component {
   state = {
     notes: ['a','aa'],
@@ -9,22 +13,34 @@ class Profile extends Component {
     },
     repos: ['r1', 'r2'],
   }
+  componentDidMount() {
+    // 为了读写数据，我们首先创建一个firebase数据库的引用
+    this.ref = new Firebase('https://github-react.firebaseio.com/');
+    // 调用child来往引用地址后面追加请求，获取数据
+    var childRef = this.ref.child(this.props.params.username);
+    // 将获取的数据转换成数组并且赋给this.state.notes
+    this.bindAsArray(childRef, 'notes');  
+  }
+  // componentWillUnMount(){
+  //   this.unbind('notes');
+  // }
   render(){
     console.log(this.props)
     return (
       <div className="row">
-        <div className="col-md-4">
-          UserProfile 路由的参数是：{this.props.params.username}
+        <div className="col-md-4">         
           <UserProfile username={this.props.params.username}
             bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          UserRepos
-          <UserRepos repos={this.state.repos} />
+          <UserRepos
+           username={this.props.params.username}
+           repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          Notes
-          <Notes notes={this.state.notes} />
+          <Notes
+           username={this.props.params.username}
+           notes={this.state.notes} />
         </div>
       </div>
     )
