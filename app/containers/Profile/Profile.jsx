9314d07849cbb6bc8c449c543ec3939604c616a1
followfork <xw@udeMacBook-Pro.local>
2016-05-08 15:29:01 +0800

    state props  传递数据

--- app/containers/Profile/Profile.jsx
+++ app/containers/Profile/Profile.jsx
@@ -2,20 +2,29 @@ import React, { Component } from 'react'
 import { UserProfile, UserRepos, Notes } from '../../components'
 
 class Profile extends Component {
+  state = {
+    notes: ['a','aa'],
+    bio: {
+      name: 'followfork',
+    },
+    repos: ['r1', 'r2'],
+  }
   render(){
+    console.log(this.props)
     return (
       <div className="row">
         <div className="col-md-4">
           UserProfile 路由的参数是：{this.props.params.username}
-          <UserProfile />
+          <UserProfile username={this.props.params.username}
+            bio={this.state.bio} />
         </div>
         <div className="col-md-4">
           UserRepos
-          <UserRepos />
+          <UserRepos repos={this.state.repos} />
         </div>
         <div className="col-md-4">
           Notes
-          <Notes />
+          <Notes notes={this.state.notes} />
         </div>
       </div>
     )
