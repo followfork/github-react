9293f1db1c10c1c7a3efd2373302c9ef74593e8e
followfork <xw@udeMacBook-Pro.local>
2016-05-08 13:54:47 +0800

    add Profile Component

--- app/containers/Home/Home.jsx
+++ app/containers/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
-class Home extends Component {
-  render() {
-    return (
-      <h2>content from Home Component</h2>
-    );
-  }
-}
+// class Home extends Component {
+//   render() {
+//     return (
+//       <h2>content from Home Component</h2>
+//     );
+//   }
+// }
 
-export default Home;
+// export default Home;
+
+export default function Home(){
+	return(
+			<h2 className="text-center">
+				通过github用户名搜索资料
+			</h2>
+		)
+}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import { App, Home, About } from '../containers';
+import { App, Home, Profile } from '../containers';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="about" component={About} />
+    <Route path="profile/:username" component={Profile} />
   </Route>
 )
